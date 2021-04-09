ff812bb01f70a6400547e1b3bd865f35a85dbb1a
xokc4 <42719177+xokc4@users.noreply.github.com>
2021-04-09 20:05:19 +0400

    Добавьте файлы проекта.

---
+++ Kursov/Forms/MenuForm.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kursov.Forms
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuForm.xaml
+    /// </summary>
+    public partial class MenuForm : Window
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void btHome_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+
+        private void btApartment_Click(object sender, RoutedEventArgs e)
+        {
+            Forms.Apartment apartment = new Apartment();
+            apartment.Show();
+            this.Close();
+        }
+
+        private void btDobKvartir_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Kursov/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kursov
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbPass.Password = "admin";
+            tbPass.PasswordChar = '*';
+            tbLog.Text = "admin";
+
+        }
+
+        private void btIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbPass.Password == "admin" && tbLog.Text == "admin")
+            {
+                Forms.MenuForm menuForm = new Forms.MenuForm();
+                menuForm.Show();
+                this.Close();
+            }
+        }
+
+        private void btInAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            Forms.MenuForm menuForm = new Forms.MenuForm();
+            menuForm.Show();
+            this.Close();
+        }
+    }
+}
